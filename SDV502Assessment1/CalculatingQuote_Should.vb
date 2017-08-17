e38c876b46e677916e993f71f171c25114799d30
William <William@CHIMAERA>
2017-08-17 19:47:00 +1200

    Add project files.

---
+++ SDV502Assessment1/CalculatingQuote_Should.vb
@@ -0,0 +1,338 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports FastwayCourier
+
+<TestClass()> Public Class CalculatingQuote_Should
+
+
+    <TestMethod()> Public Sub Return4Point15And0_IfWeightIs12Point5AndZoneIsPink()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (12.5)
+        Dim Zone As String = ("Pink")
+        Dim Expected = 4.15 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub Return4Point15And0_IfWeightIs0Point01AndZoneIsPink()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (0.01)
+        Dim Zone As String = ("Pink")
+        Dim Expected = 4.15 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub Return4Point15And0_IfWeightIs25AndZoneIsPink()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (25)
+        Dim Zone As String = ("Pink")
+        Dim Expected = 4.15 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod, ExpectedException(GetType(System.ArgumentOutOfRangeException))> Public Sub ReturnThrowException_IfWeightIsMinus1AndZoneIsPink()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (-1)
+        Dim Zone As String = ("Pink")
+        Dim Expected = 4.15 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod, ExpectedException(GetType(System.ArgumentOutOfRangeException))> Public Sub ReturnThrowException_IfWeightIs26AndZoneIsPink()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (26)
+        Dim Zone As String = ("Pink")
+        Dim Expected = 4.15 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+
+
+
+
+
+    <TestMethod()> Public Sub Return6Point95And0_IfWeightIs12Point5AndZoneIsBlue()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (12.5)
+        Dim Zone As String = ("Blue")
+        Dim Expected = 4.15 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+    
+    <TestMethod()> Public Sub Return6Point95And0_IfWeightIs0Point01AndZoneIsBlue()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (0.01)
+        Dim Zone As String = ("Blue")
+        Dim Expected = 4.15 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub Return6Point95And0_IfWeightIs25AndZoneIsBlue()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (25)
+        Dim Zone As String = ("Blue")
+        Dim Expected = 4.15 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod, ExpectedException(GetType(System.ArgumentOutOfRangeException))> Public Sub ReturnThrowException_IfWeightIsMinus1AndZoneIsBlue()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (-1)
+        Dim Zone As String = ("Blue")
+        Dim Expected = 4.15 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod, ExpectedException(GetType(System.ArgumentOutOfRangeException))> Public Sub ReturnThrowException_IfWeightIs26AndZoneIsBlue()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (26)
+        Dim Zone As String = ("Blue")
+        Dim Expected = 4.15 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+
+
+
+
+
+    <TestMethod()> Public Sub Return8Point70And0_IfWeightIs12Point5AndZoneIsLime()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (12.5)
+        Dim Zone As String = ("Lime")
+        Dim Expected = 8.7 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub Return8Point70And0_IfWeightIs0Point01AndZoneIsLime()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (0.01)
+        Dim Zone As String = ("Lime")
+        Dim Expected = 8.7 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub Return14Point90And1_IfWeightIs25AndZoneIsLime()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (25)
+        Dim Zone As String = ("Lime")
+        Dim Expected = 14.9 And 1
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod, ExpectedException(GetType(System.ArgumentOutOfRangeException))> Public Sub ReturnThrowException_IfWeightIsMinus1AndZoneIsLime()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (-1)
+        Dim Zone As String = ("Lime")
+        Dim Expected = 8.7 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod, ExpectedException(GetType(System.ArgumentOutOfRangeException))> Public Sub ReturnThrowException_IfWeightIs26AndZoneIsLime()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (26)
+        Dim Zone As String = ("Lime")
+        Dim Expected = 14.9 And 1
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+
+
+
+
+
+    <TestMethod()> Public Sub Return12Point95And0_IfWeightIs12Point5AndZoneIsOrange()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (12.5)
+        Dim Zone As String = ("Orange")
+        Dim Expected = 12.95 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub Return12Point95And0_IfWeightIs0Point01AndZoneIsOrange()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (0.01)
+        Dim Zone As String = ("Orange")
+        Dim Expected = 12.95 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub Return19Point15And1_IfWeightIs16AndZoneIsOrange()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (16)
+        Dim Zone As String = ("Orange")
+        Dim Expected = 19.15 And 1
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub Return25Point35And2_IfWeightIs25AndZoneIsOrange()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (25)
+        Dim Zone As String = ("Orange")
+        Dim Expected = 25.35 And 2
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod, ExpectedException(GetType(System.ArgumentOutOfRangeException))> Public Sub ReturnThrowException_IfWeightIsMinus1AndZoneIsOrange()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (-1)
+        Dim Zone As String = ("Orange")
+        Dim Expected = 12.95 And 0
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod, ExpectedException(GetType(System.ArgumentOutOfRangeException))> Public Sub ReturnThrowException_IfWeightIs26AndZoneIsOrange()
+        'Arrange
+        Dim Shipping As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Quote As ParcelQuoteResult = New ParcelQuoteResult
+        Dim Weight As Decimal = (26)
+        Dim Zone As String = ("Orange")
+        Dim Expected = 25.35 And 2
+        Dim Actual
+        'Act
+        Shipping.CalculateQuote(Weight, Zone)
+        Actual = Quote.Price And Quote.ExcessTickets
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+End Class
---
+++ SDV502Assessment1/GettingDestinationZone_Should.vb
@@ -0,0 +1,287 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports FastwayCourier
+
+<TestClass()> Public Class GettingDestinationZone_Should
+
+    <TestMethod()> Public Sub returnBlue_IfDestinationIsHavelock()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Havelock"
+        Dim Expected As String = "Blue"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnBlue_IfDestinationIsRiwaka()
+
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Riwaka"
+        Dim Expected As String = "Blue"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnBlue_IfDestinationIsSeddon()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Seddon"
+        Dim Expected As String = "Blue"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnBlue_IfDestinationIsTakaka()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Takaka"
+        Dim Expected As String = "Blue"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnBlue_IfDestinationIsWaihopaiValley()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Waihopai Valley"
+        Dim Expected As String = "Blue"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnBlue_IfDestinationIsWard()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Ward"
+        Dim Expected As String = "Blue"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+
+
+
+
+
+    <TestMethod()> Public Sub returnLime_IfDestinationIsMurchison()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Murchison"
+        Dim Expected As String = "Lime"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnLime_IfDestinationIsNelsonLakesNationalPark()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Nelson Lakes National Park"
+        Dim Expected As String = "Lime"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+
+
+
+
+
+    <TestMethod()> Public Sub returnOrange_IfDestinationIsHanmerSprings()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Hanmer Springs"
+        Dim Expected As String = "Orange"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnOrange_IfDestinationIsKaikoura()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Kaikoura"
+        Dim Expected As String = "Orange"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnOrange_IfDestinationIsReefton()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Reefton"
+        Dim Expected As String = "Orange"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+
+
+
+
+
+    <TestMethod()> Public Sub returnPink_IfDestinationIsAtawhai()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Atawhai"
+        Dim Expected As String = "Pink"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnPink_IfDestinationIsBlenheim()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Blenheim"
+        Dim Expected As String = "Pink"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnPink_IfDestinationIsBrightwater()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Brightwater"
+        Dim Expected As String = "Pink"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnPink_IfDestinationIsHope()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Hope"
+        Dim Expected As String = "Pink"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnPink_IfDestinationIsMaitai()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Maitai"
+        Dim Expected As String = "Pink"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnPink_IfDestinationIsMapua()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Mapua"
+        Dim Expected As String = "Pink"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnPink_IfDestinationIsMotueka()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Motueka"
+        Dim Expected As String = "Pink"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnPink_IfDestinationIsNelson()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Nelson"
+        Dim Expected As String = "Pink"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnPink_IfDestinationIsPicton()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Picton"
+        Dim Expected As String = "Pink"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnPink_IfDestinationIsRenwick()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Renwick"
+        Dim Expected As String = "Pink"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+    <TestMethod()> Public Sub returnPink_IfDestinationIsWakefield()
+        'Arrange
+        Dim Zone As ParcelQuoteFromNelson = New ParcelQuoteFromNelson
+        Dim Destination As String = "Wakefield"
+        Dim Expected As String = "Pink"
+        Dim Actual As String
+        'Act
+        Actual = Zone.GetDestinationZone(Destination)
+        'Assert
+        Assert.AreEqual(Expected, Actual)
+    End Sub
+
+End Class
